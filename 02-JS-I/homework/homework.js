5f897659b031f7f8972da0f4848ac57b281c02ec
elrufo21 <neburtnt@gmail.com>
2021-12-28 23:09:06 -0500

    Second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola perros';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 52;
 
 // Crea una variable booleana:
 const nuevoBool = null;
